d5e22f0dd43036262856df6e1d76b9be3e89b651
malcolmkibel-bit <malcolmkibel@students.berkeley.net>
2026-01-28 18:54:43 +0000

    hi

--- script.js
+++ script.js
@@ -1,9 +1,60 @@
 //starter code
 
+
 function run() {
-    sleep("Tigger");
+  var tigger = new Tiger("Tigger");
+  tigger("tigger", "meat");
+
+  var beer = new Bear("pooh")
+  beer("pooh", "fish")
+}
+
+class Tiger extends Animal{
+
+  constructor(name) {
+   super(name, "meat");
   }
-  
-  function sleep(name) {
-    console.log(name + " sleeps for 8 hours")
+}
+
+
+
+
+
+class Animal {
+    
+    constructor(name,favoriteFood) {
+        this.name = name;
+        this.favoriteFood = favoriteFood;
+    }
+
+    sleep() {
+        console.log(this.name + " sleeps for 8 hours");
   }
+
+    }
+
+    eat(food) {
+      console.log(this.name + " eats " + food)
+    if (food == this.favoriteFood) {
+      console.log("YUM!!! " + this.name + " wants more " + food);
+    } else {
+      this.sleep();
+    }
+
+    }
+}
+
+
+
+
+
+class Bear extends Animal {
+
+  constructor(name, favoriteFood) {
+    super(name, "fish")
+  }
+}
+
+
+
+run();
